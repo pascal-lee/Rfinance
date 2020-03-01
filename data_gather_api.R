4779a99b1b4f69469cae5291f7fa42237b0033b3
pascal <bseoklee@gmail.com>
2020-03-01 20:26:07 +0900

    add library symple backtest

--- data_gather_api.R
+++ data_gather_api.R
@@ -1,5 +1,12 @@
-pkg<-c('magrittr','quantmod','rvest','httr','jsonlite')
+pkg<-c('magrittr','quantmod','rvest','httr','jsonlite',
+       'readr','readxl','stringr','lubridate','dpyr',
+       'tidyr','ggplot2','corrplot','dygraphs',
+       'highcharter','plotly','PerformanceAnalytics',
+       'nlopter','quadprog','riskPortfolios','cccp',
+       'timetk','bromm','stargazer')
 new.pkg <-pkg[!pkg %in% installed.packages()[,"Package"]]
 if (length(new.pkg)){
   install.packages(new.pkg,dependencies = TRUE)
 }
+
+                                                                            
