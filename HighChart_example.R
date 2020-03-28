421cbba5b0d2aed40a8e47ce734bc2f952b2d5e5
pascal-lee <bseoklee@gmail.com>
2020-03-28 20:34:04 +0900

    ticker add

--- HighChart_example.R
+++ HighChart_example.R
@@ -15,7 +15,40 @@ new.pkg <-pkg[!pkg %in% installed.packages()[,"Package"]]
 if (length(new.pkg)){
   install.packages(new.pkg,dependencies = TRUE)
 }
-symbols<-c('SPY','EFA','IJS','EEM','AGG')
+symbols <- c("XLB", #SPDR Materials sector
+             "XLE", #SPDR Energy sector
+             "XLF", #SPDR Financial sector
+             "XLP", #SPDR Consumer staples sector
+             "XLI", #SPDR Industrial sector
+             "XLU", #SPDR Utilities sector
+             "XLV", #SPDR Healthcare sector
+             "XLK", #SPDR Tech sector
+             "XLY", #SPDR Consumer discretionary sector
+             "RWR", #SPDR Dow Jones REIT ETF
+             "EWJ", #iShares Japan
+             "EWG", #iShares Germany
+             "EWU", #iShares UK
+             "EWC", #iShares Canada
+             "EWY", #iShares South Korea
+             "EWA", #iShares Australia
+             "EWH", #iShares Hong Kong
+             "EWS", #iShares Singapore
+             "IYZ", #iShares U.S. Telecom
+             "EZU", #iShares MSCI EMU ETF
+             "IYR", #iShares U.S. Real Estate
+             "EWT", #iShares Taiwan
+             "EWZ", #iShares Brazil
+             "EFA", #iShares EAFE
+             "IGE", #iShares North American Natural Resources
+             "EPP", #iShares Pacific Ex Japan
+             "LQD", #iShares Investment Grade Corporate Bonds
+             "SHY", #iShares 1-3 year TBonds
+             "IEF", #iShares 3-7 year TBonds
+             "TLT" ,#iShares 20+ year Bonds,
+             'TIPS',
+)
+
+symbols<-c('SPY','EFA','IJS','EEM','AGG','005930.KS')
 
 prices<-
   getSymbols(symbols,
@@ -63,7 +96,12 @@ highchart(type = 'stock') %>%
                 color = 'green')
 
 highchart(type = 'stock') %>% 
-  hc_add_series(asset_return_xts$SPY, type='column')
+  hc_add_series(asset_return_xts$SPY, type='column',name='SPY') %>% 
+  hc_add_series(asset_return_xts$EEM, type='column',name='EEM') %>%
+  hc_add_series(asset_return_xts$`005930.KS`, type='column',name='SS') %>% 
+  hc_tooltip(pointFormat ='{point.x: %Y-%m-%d}
+            {point.y:.4f}%'
+             )
 
 highchart(type = 'stock') %>% 
   hc_add_series(asset_return_xts$SPY, type='scatter') %>% 
@@ -76,6 +114,13 @@ highchart(type = 'stock') %>%
             {point.y:.4f}%'
 )
 
+highchart(type = 'stock') %>% 
+  hc_add_series(asset_return_xts$SPY, type='scatter', name='SPY') %>% 
+  hc_add_series(asset_return_xts$EFA, type='scatter',name='EFA') %>% 
+  hc_add_series(asset_return_xts$`005930.KS`, type='scatter',name='005930.KS') %>% 
+  hc_tooltip(pointFormat ='{point.x: %Y-%m-%d}
+            {point.y:.4f}%'
+  )
 
 highchart(type = 'stock') %>% 
   hc_add_series(asset_return_xts$SPY, type='scatter', name='SPY') %>% 
@@ -85,8 +130,8 @@ highchart(type = 'stock') %>%
 
 head(asset_return_xts)
 head(asset_returns_long)
-prices = getSymbols("IBM", from = "2010-01-01", to = "2020-03-06")
-R.IBM = Return.calculate(xts(prices), method="discrete")
+prices_IBM = getSymbols("IBM", from = "2010-01-01", to = "2020-03-06")
+R.IBM = Return.calculate(xts(prices_IBM), method="discrete")
 colnames(R.IBM)="IBM"
 chart.CumReturns(R.IBM,legend.loc="topleft", main="Cumulative Daily Returns for IBM")
 round(R.IBM,2)
